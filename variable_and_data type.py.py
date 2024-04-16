7b5287c0ab18e2cfe880df88f96b32d4c71372fb
Vaibhav336699 <167185998+Vaibhav336699@users.noreply.github.com>
2024-04-16 14:50:25 +0530

    Add files via upload

---
+++ variable_and_data type.py.py
@@ -0,0 +1,47 @@
+#                  VARIABLE AND DATA TYPES
+
+ # 1)WAP TO PRINT HELLO
+
+#print("hello")
+
+# 2) wap to print addition of two numbers
+
+#num1=int(input("Enter number 1 = "))
+#num2=int(input("Enter number 2 = "))
+#print("Addition= ",num1+num2)
+
+# 3)wap to calculate simple interest taking input from uses.
+"""p= float(input("Enter principal = "))
+r= int(input("Enter rate= "))
+t= int(input("Enter time period= "))
+si= p*r*t/100
+print("simple interest is  = ",si)
+"""""
+
+
+""" 4)WAP TO CALCULATE AREA OF RECTANGLE
+len= int(input("Enter length of rectangle = "))
+wid= int(input("Enter width of rectangle = "))
+print("Area of rectangle is = ",len*wid)""
+"""
+
+# 5)WAP TO CALCULATE CUBE OF ENTERED NUMBER
+"""x=int(input("Enter a number = "))
+print("cube is = ",x**x)
+"""
+
+# 6)WAP A PROGRAM TO TAKE ANY EMPLOYEE DETAILS AND DISPLAY IT
+"""empid= int(input("Enter your employee id = "))
+empname= input("Enter your name = ")
+print("Employee ID is",empid)
+print("Employee name is",empname)
+
+"""
+
+# 7)wap to take a number from user decrement the value by 3
+number= int(input("enter a number = "))
+number-=3
+print("Decreased value of the number is ",number)
+
+
+
